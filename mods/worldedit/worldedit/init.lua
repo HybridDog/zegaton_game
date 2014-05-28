4d2f58ee338e78af485f79d2740236338572cbe0
HybridDog <adresse_test@t-online.de>
2014-05-28 23:27:07 +0200

    use submodules
    add mods

--- mods/worldedit/worldedit/init.lua
+++
@@ -1,4 +0,0 @@
-dofile(minetest.get_modpath("worldedit") .. "/manipulations.lua")
-dofile(minetest.get_modpath("worldedit") .. "/primitives.lua")
-dofile(minetest.get_modpath("worldedit") .. "/visualization.lua")
-dofile(minetest.get_modpath("worldedit") .. "/serialization.lua")
